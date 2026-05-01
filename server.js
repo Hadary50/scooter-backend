21273f9e986487dbde65f99c51c2e2a958c0177d
Hadary50 <Hadary465@gmai.com>
2026-05-01 17:45:00 +0300

    Fix CORS for authentication

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
